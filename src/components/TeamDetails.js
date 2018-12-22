da1708449271821b7cee220facea7762ed82cc42
Erica Bee <ebee511@gmail.com>
2018-12-22 14:30:13 -0700

    Pass props and use a console statement to check passing.

--- src/components/TeamDetails.js
+++ src/components/TeamDetails.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const TeamDetails = props => (
-	<p>Hello World</p>
-);
+const TeamDetails = (props) => {
+	console.log(props);
+
+	return (
+		<div className='App'>
+			<header className="sports_header">
+		    <h1 className="sports_header_title">NBA Team Encyclopedia</h1>
+		  </header>
+		  <main>
+		  	<h2>Hello</h2>
+		  </main>
+		</div>
+	)
+}
+
 
 export default TeamDetails;
