a0711dacec6d93ad0d87503b9a4e365023830355
Erica Bee <ebee511@gmail.com>
2019-01-02 21:02:38 -0700

    Remove unused import.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Teams from './components/Teams.js';
-import TeamDetails from './components/TeamDetails.js';
 import './App.css';
 
 class App extends Component {
